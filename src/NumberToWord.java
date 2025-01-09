4d21b393035dc18c58a18582bb41b3126d03ce89
Gerogie18 <jennlyver@gmail.com>
2025-01-09 14:24:30 -0330

    Lab 1: Number to word

---
+++ src/NumberToWord.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NumberToWord {
+    public static void main(String[] args) {
+        String[] numberWords = {"one", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a one-digit number (0-9): ");
+        int number = scanner.nextInt();
+
+        if (number < 0 || number > 9) {
+            System.out.println("Error: Invalid Number.");
+        } else {
+            System.out.println(numberWords[number]);
+        }
+        scanner.close();
+    }
+}
