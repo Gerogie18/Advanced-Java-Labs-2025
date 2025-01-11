00155d15f1bd03877ad3036a88d74cc513b0a630
Gerogie18 <jennlyver@gmail.com>
2025-01-10 23:02:16 -0330

    Create SwitchCalc.java

---
+++ src/Week1/SwitchCalc.java
@@ -0,0 +1,45 @@
+package Week1;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SwitchCalc {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input simple math expression with two operators");
+        String input = scanner.nextLine().replaceAll("\\s", "");
+
+        Pattern pattern = Pattern.compile("(\\d+(?:\\.\\d+)?)([+\\-x*/])(\\d+(?:\\.\\d+)?)");
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.matches()) {
+            double operandOne = Double.parseDouble(matcher.group(1));
+            String operator = matcher.group(2);
+            double operandTwo = Double.parseDouble(matcher.group(3));
+
+            switch (operator) {
+                case "+":
+                    System.out.println("Addition - Result:");
+                    System.out.println((operandOne + operandTwo));
+                    break;
+                case "-":
+                    System.out.println("Subtraction - Result:");
+                    System.out.println((operandOne - operandTwo));
+                    break;
+                case "/":
+                    System.out.println("Division - Result:");
+                    System.out.println((operandOne / operandTwo));
+                    break;
+                case "*":
+                case "x":
+                    System.out.println("Multiplication - Result:");
+                    System.out.println((operandOne * operandTwo));
+                    break;
+            }
+        } else {
+            System.out.println("Input invalid: please input simple expression\n    Such as \" 1 + 1 \" ");
+        }
+    }
+}
